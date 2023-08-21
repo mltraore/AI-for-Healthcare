d9fbea6503281859150096bbd351bd30d87ad4e7
mohamedlassine97 <mohamedlassine97@gmail.com>
2023-08-21 14:40:13 +0300

    Add Section2: Training a Segmentation CNN to Project 2

---
+++ Project 2: Quatifying Hippocampal Volume in Alzheimer's Progression/section2/src/utils/volume_stats.py
@@ -0,0 +1,69 @@
+"""
+Contains various functions for computing statistics over 3D volumes
+"""
+import numpy as np
+
+def Dice3d(a, b):
+    """
+    This will compute the Dice Similarity coefficient for two 3-dimensional volumes
+    Volumes are expected to be of the same size. We are expecting binary masks -
+    0's are treated as background and anything else is counted as data
+
+    Arguments:
+        a {Numpy array} -- 3D array with first volume
+        b {Numpy array} -- 3D array with second volume
+
+    Returns:
+        float
+    """
+    if len(a.shape) != 3 or len(b.shape) != 3:
+        raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+
+    if a.shape != b.shape:
+        raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+
+    # TASK: Write implementation of Dice3D. If you completed exercises in the lessons
+    # you should already have it.
+    # <YOUR CODE HERE>
+    
+    # 2*|gt ∩ pred| / |gt|+|pred|
+    intersection = np.sum(np.logical_and(a == b, a != 0)) # np.sum(a * b)
+    sum_ = np.sum(a != 0) + np.sum(b != 0)
+
+    if sum_ == 0:
+        return -1
+    
+    return 2. * float(intersection) / float(sum_)
+    
+
+def Jaccard3d(a, b):
+    """
+    This will compute the Jaccard Similarity coefficient for two 3-dimensional volumes
+    Volumes are expected to be of the same size. We are expecting binary masks - 
+    0's are treated as background and anything else is counted as data
+
+    Arguments:
+        a {Numpy array} -- 3D array with first volume
+        b {Numpy array} -- 3D array with second volume
+
+    Returns:
+        float
+    """
+    if len(a.shape) != 3 or len(b.shape) != 3:
+        raise Exception(f"Expecting 3 dimensional inputs, got {a.shape} and {b.shape}")
+
+    if a.shape != b.shape:
+        raise Exception(f"Expecting inputs of the same shape, got {a.shape} and {b.shape}")
+
+    # TASK: Write implementation of Jaccard similarity coefficient. Please do not use 
+    # the Dice3D function from above to do the computation ;)
+    # <YOUR CODE GOES HERE>
+    
+    # |gt ∩ pred| / |gt ∪ pred|
+    intersection = np.sum(np.logical_and(a == b, a != 0))
+    union = np.sum(np.logical_or(a != 0, b != 0))
+    
+    if union == 0:
+        return -1
+    
+    return float(intersection) / float(union)
